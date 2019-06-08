fbdf5280b4fde365b7a72fe1be7948d95c0ed085
AVZ <alex.zelenskiy@yahoo.com>
2019-06-08 12:28:42 +0300

    Just tests of ReactJS

---
+++ src/components/items-list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ItemsList = () => {
+    return (
+        <ul>
+            <li>Drink tea</li>
+            <li>Eat breakfast</li>
+            <li>Walk at street</li>
+        </ul>
+    )
+};
+
+export default ItemsList;
